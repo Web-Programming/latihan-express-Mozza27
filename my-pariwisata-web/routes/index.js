2d62e58556524cc28e1a0828208d6ce9cf7a1eb6
Mozza27 <sitisheilamozza_2226240168@mhs.mdp.ac.id>
2024-10-04 17:01:54 +0700

    Aplikasi Paket Wisata

---
+++ my-pariwisata-web/routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+router.get('/paketwisata', function(req, res, next) {
+  res.render('paketwisata', { title: 'Paket Wisata' });
+});
+router.get('/orderpaket', function(req, res, next) {
+  res.render('orderpaket', { title: 'Order Paket Wisata' });
+});
+router.get('/orderdetail', function(req, res, next) {
+  res.render('orderdetail', { title: 'Detail Pemesanan Paket Wisata' });
+});
+
+
+module.exports = router;
